c391a3a8e1988eb0fe262737bd7ab30a0aa064fe
Ahad <mohdahad60080@gmail.com>
2025-10-10 00:05:34 +0530

    docs

--- ground_zero/variables.py
+++ ground_zero/variables.py
@@ -1,9 +1,12 @@
+# Step 1: Create variables
 likes = 40
 comments = 15
 shares = 5
 
+# Step 2: Calculate total engagement (expression stored in variable)
 total_engagement = likes + comments + shares
 
+# Step 3: Print everything (statements)
 print("Post Stats:")
 print("Likes:", likes)
 print("Comments:", comments)
