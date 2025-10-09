f79a376e24d36862bc0a71990e5eaf9171651f6a
Ahad <mohdahad60080@gmail.com>
2025-10-10 00:04:30 +0530

    hmm

---
+++ ground_zero/variables.py
@@ -0,0 +1,11 @@
+likes = 40
+comments = 15
+shares = 5
+
+total_engagement = likes + comments + shares
+
+print("Post Stats:")
+print("Likes:", likes)
+print("Comments:", comments)
+print("Shares:", shares)
+print("Total Engagement:", total_engagement)
