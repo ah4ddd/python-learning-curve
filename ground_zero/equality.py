e8c2b08af60ecd7b5504c319c08b26d75f2ad176
Ahad <mohdahad60080@gmail.com>
2025-10-10 16:07:11 +0530

    ==

---
+++ ground_zero/equality.py
@@ -0,0 +1,13 @@
+name1 = "Ahad"
+name2 = "Ahad"
+
+print(name1 == name2)  # True
+
+saved_pin = 1234
+entered_pin = int(input("Enter your PIN: "))
+
+if entered_pin == saved_pin:
+    print("Access granted")
+else:
+    print("Access denied")
+
