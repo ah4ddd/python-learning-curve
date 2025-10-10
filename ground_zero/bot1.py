9b75565dfe4dc17aa420077863220fd96e5a686e
Ahad <mohdahad60080@gmail.com>
2025-10-10 18:14:40 +0530

    P1mimo-s

---
+++ ground_zero/bot1.py
@@ -0,0 +1,11 @@
+print ("Hello! what is your name?")
+name = "Ahad"
+print (f"Nice to meet you, {name}!")
+print ("How old are you?")
+age = 20
+bot_age = 3
+age_difference = age - bot_age
+print (f"You are {age_difference} years older than me. I'm only {bot_age} years old!")
+print ("What's your favorite color?")
+color = "black"
+print (f"Oh {color} is a beautiful color")
