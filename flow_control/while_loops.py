857abed4b30c4a858fec9eccadcd76a507b9879c
Ahad <mohdahad60080@gmail.com>
2025-10-13 17:31:13 +0530

    brain melting lil shit

--- flow_control/while_loops.py
+++ flow_control/while_loops.py
@@ -1,9 +1,3 @@
-counter = 1
-
-while counter <= 5:
-    print(f"Counter is at {counter}")
-    counter += 1
-
 password = ""
 
 while password != "python123":
@@ -12,6 +6,3 @@ while password != "python123":
         print("Wrong! Try again...")
 
 print("Access granted!")
-
-while True:
-    print("This will never stop!")
