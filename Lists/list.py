61e8b5a3ea76959132dd128f6d89d5cc0ebe3a90
Ahad <mohdahad60080@gmail.com>
2025-10-14 20:58:14 +0530

    smarter way

--- Lists/list.py
+++ Lists/list.py
@@ -1,11 +1,8 @@
 favorite_films = ["city of god", "in the mood for love", "incendies", "come and see", "paris texas"]
 favorite_films.append("la haine")
-print(favorite_films[0])
-print(favorite_films[1])
-print(favorite_films[2])
-print(favorite_films[3])
-print(favorite_films[4])
-print(favorite_films[5])
+
+for film in favorite_films:
+    print(film)
 
 print(len(favorite_films))
 
