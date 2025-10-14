9d87ad96bd014bc94cb6bf5ec7607343dcc74829
Ahad <mohdahad60080@gmail.com>
2025-10-14 21:27:43 +0530

    changing data in lists

--- Lists/list.py
+++ Lists/list.py
@@ -1,8 +1,24 @@
 favorite_films = ["city of god", "in the mood for love", "incendies", "come and see", "paris texas"]
+
 favorite_films.append("la haine")
+
+favorite_films[5] = "stalker"
+
+favorite_films.append("the 400 blows")
+
+favorite_films.insert(2, "mulholland Drive")
+
+favorite_films.remove("incendies")
+
+favorite_films.pop(3)
+
+removed_film = favorite_films.pop(3)
 
 for film in favorite_films:
     print(film)
 
+print(f"Removed: {removed_film}")
+
 print(len(favorite_films))
+
 
