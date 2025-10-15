39f2f9d535385e31c30bb44a599e69a9b5bbe8a5
Ahad <mohdahad60080@gmail.com>
2025-10-15 20:40:42 +0530

    for me

--- Lists/mxsteplag.py
+++ Lists/mxsteplag.py
@@ -8,3 +8,14 @@ print("Steps from highest to lowest:", sorted_steps)
 
 max_steps_day = days[steps.index(max(steps))]
 print(f"Hottest step day: {max_steps_day} with {max(steps)} steps")
+
+
+# max(steps) → finds the biggest number, e.g. 9000.
+
+# steps.index(max(steps)) → finds the position of that number in the list → 4.
+
+# days[4] → uses that position to grab the day name from the days list → "Fri".
+
+# So max_steps_day = days[steps.index(max(steps))]
+
+# → means “give me the day that matches the highest step count.”
