10cb18c87b147eb95364f9c2833edac27ad777a1
Ahad <mohdahad60080@gmail.com>
2025-10-15 20:11:42 +0530

    listing

--- Lists/list.py
+++ Lists/list.py
@@ -1,11 +1,14 @@
-items = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  # 10 items total
+life_experience = ["sex", "drugs", "rock n roll", "travel", "adventure", "food", "friends", "family"]
 
-first_half = items[1:4]   # Gets items 0,1,2,3,4
-second_half = items[5:10]  # Gets items 5,6,7,8,9
+life_experience.append("learning")
+life_experience.insert(0, "fun")
+life_experience.remove("rock n roll")
+life_experience.pop(7)
+life_experience[3] = "exploration"
+life_experience[2:4] = ["culture", "nature"]
+life_experience_sorted = sorted(life_experience)
+life_experience.sort(reverse=True)
 
-print("First half:", first_half)
-print("Second half:", second_half)
-
-word = "incredible"
-print(word[2:7])  # indexes 2,3,4,5,6 → 'cred' + 'i'?
-# indexes 2 to 6 included, 7 excluded
+print(life_experience)
+print(life_experience_sorted)
+print(life_experience[1:4])
