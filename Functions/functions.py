c50ea5d53c53adcd823d7e825cdb1177d7826a86
Ahad <mohdahad60080@gmail.com>
2025-10-22 14:10:43 +0530

    bsic

---
+++ Functions/functions.py
@@ -0,0 +1,8 @@
+def calculate_area(length, width):
+    area = length * width
+    return area
+
+# Now just call it!
+print(calculate_area(10, 5))   # 50
+print(calculate_area(20, 8))   # 160
+print(calculate_area(15, 12))  # 180
