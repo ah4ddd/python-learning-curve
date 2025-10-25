d589a7a8c0af9baa7a5143ddfa90c448413199fc
Ahad <mohdahad60080@gmail.com>
2025-10-25 21:38:24 +0530

    condition

--- Functions/functions.py
+++ Functions/functions.py
@@ -1,12 +1,32 @@
-# This is GLOBAL
-greeting = "Hello"
+def check_temperature(temp):
+    if temp > 30:
+        print("It's hot outside! 🥵")
+    elif temp >= 20:
+        print("Nice and warm ☀️")
+    else:
+        print("It's cold! 🥶")
 
-def say_hello():
-    print(greeting)
+check_temperature(3)
 
-def say_goodbye():
-    print(greeting)
+def grade_student(score):
+    if score >= 90:
+        return "A"
+    elif score >= 75:
+        return "B"
+    elif score >= 50:
+        return "C"
+    else:
+        return "F"
 
-say_hello()
-say_goodbye()
-print(greeting)
+result = grade_student(35)
+print(f"your result is {result} dogshit")
+
+def can_vote(age, citizen):
+    if age >= 18 and citizen == True:
+        print("You can vote! 🗳️")
+    elif age < 18:
+        print("Too young to vote.")
+    else:
+        print("You’re not a citizen, sorry!")
+
+can_vote(20,False)
