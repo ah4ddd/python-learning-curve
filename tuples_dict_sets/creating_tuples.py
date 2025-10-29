8b3c2fc5c13c07dd3407df90ec26218e1bcdc917
Ahad <mohdahad60080@gmail.com>
2025-10-29 22:50:23 +0530

    list + tuples

--- tuples_dict_sets/creating_tuples.py
+++ tuples_dict_sets/creating_tuples.py
@@ -14,22 +14,22 @@ else:
     print("\n❌ Not in the list.")
 
 playlist = (
-    ("Creep", "Radiohead"),
-    ("Smells Like Teen Spirit", "Nirvana"),
-    ("Paranoid", "Black Sabbath"),
-    ("Fade to Black", "Metallica"),
-    ("Karma Police", "Radiohead")
+    ("Creep", "Radiohead", 1993),
+    ("Smells Like Teen Spirit", "Nirvana", 1991),
+    ("Paranoid", "Black Sabbath", 1970),
+    ("Fade to Black", "Metallica", 1984)
 )
 
 print("\n🎧 My Playlist:")
-for song, artist in playlist:
+for song, artist, year in playlist:
  print("\n🎧 My Playlist:")
-for song, artist in playlist:
-    print(f"{song} — by {artist}")
+
+for song, artist, year in playlist:
+    print(f"{song} — by {artist} in {year}")
 
 search_artist = "Radiohead"
 
 print(f"\n🔎 Songs by {search_artist}:")
-for song, artist in playlist:
+for song, artist, year in playlist:
     if artist == search_artist:
         print("-", song)
--- tuples_dict_sets/tuples_dict_sets.py
+++ tuples_dict_sets/tuples_dict_sets.py
@@ -1,91 +1,21 @@
-F = "f","s","o","c","i","e","t","y"
+game = ("sekiro", "elden ring", "mgs")
 
-print(F)
+game_list = list(game)
 
-lst = [1,2,3]
-t = tuple(lst)
-print(t)
+print(game_list)
 
-z = 1; n =4; j = 8;
-packed = z, n, j
+game = ("sekiro", "elden ring", "mgs")
 
-print (packed)
+game_list = list(game)
 
-x, c, v = (34, 35, 36)
+print(game_list)
 
-print (x, c, v)
+game_list[1] = "silent_hill"
 
-tupe = ("fun", "not", "for", "me", "not")
+game = tuple(game_list)
 
-tupe[0]
+print(game)
 
-for t in range (len(tupe)):
-    print(tupe[t])
+game = tuple(game_list)
 
-not_count = tupe.count("not")
-
-print (not_count)
-
-indx = tupe.index("me")
-print(indx)
-
-person = ("Ahad", 22, "India")
-name, age, country = person
-
-print (person)
-
-head, *middle, tail = (1,2,3,4,5)
-print (head)
-
-matrix = ((1,2,3), (4,5,6), (7,8,9))
-print (matrix[2][2])
-
-print(max(matrix))
-print(min(matrix))
-
-def stats(nums):
-    s = sum(nums)
-    n = len(nums)
-    mean = s / n
-    print(mean)
-    return s, n, mean
-
-total, count, average = stats([10,20,30])
-
-coord = (28.6139, 77.2090)
-lat, lon = coord
-
-print("Latitude:", lat)
-print("Longitude:", lon)
-
-visits = {}
-location = (51.5074, -0.1278)
-visits[location] = "London"
-
-print (visits)
-
-colors = ("red", "green", "white")
-print(colors[1])
-
-def min_max(nums):
-    print(max(nums))
-    print(min(nums))
-
-min_max((12, 30, 40, 50, 900))
-
-HOME = (3,6)
-BIKE = (2,4)
-BAR = (5,10)
-
-coordinates = [HOME, BIKE, BAR]
-
-map = {
-    HOME: "HOME",
-    BIKE: "BIKE",
-    BAR: "BAR"
-}
-
-data = (1,2,3,4,5)
-print (data)
-z, *b, f = data
-print((f,*b,z))
+print(game)
