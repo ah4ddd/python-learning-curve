3e1faf95fa61c8790eab937d037b99906b81f6b8
Ahad <mohdahad60080@gmail.com>
2025-10-29 19:03:33 +0530

    too boring

--- tuples_dict_sets/creating_tuples.py
+++ tuples_dict_sets/creating_tuples.py
@@ -1,39 +1,26 @@
-import math
+menu =[
+    ("burger", 120),
+    ("Pizza", 250),
+    ("Pasta", 150),
+    ("Fries", 100),
+    ("Coke", 70)
+]
 
-DELHI = (28.6139, 77.2090)
-MUMBAI = (19.0760, 72.8777)
-KOLKATA = (22.5726, 88.3639)
-CHENNAI = (13.0827, 80.2707)
+print (" Welcome to python Cafe")
+print("Here is our Menu:\n")
 
-city_info = {
-    DELHI: "Capital of India",
-    MUMBAI: "Financial hub and home to Bollywood",
-    KOLKATA: "Cultural capital of India",
-    CHENNAI: "Gateway to South India"
-}
+for item in menu:
+    print(f"{item[0]} - {item[1]}")
 
-def show_all_cities():
-    print("\n🌍 List of Cities:")
-    for coord, info in city_info.items():
-        print(f"City: {info} | Coordinates: {coord}")
+order = input("\nWhat'd you like to order? ")
 
-def find_city(coord):
-    return city_info.get(coord, "City not found")
+found = False
+for item in menu:
+    if order.lower() == item[0].lower():
+        print(f"\nYou ordered {item[0]} — that will be ₹{item[1]}.")
+        found = True
+        break
 
-def calculate_distance(coord1, coord2):
-    lat1, lon1 = map(math.radians, coord1)
-    lat2, lon2 = map(math.radians, coord2)
+if not found:
+    print("\nSorry, we don’t have that on our menu today.")
 
-    dlat = lat2 - lat1
-    dlon = lon2 - lon1
-    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
-    c = 2 * math.asin(math.sqrt(a))
-    r = 6371
-    return round(c * r, 2)
-
-show_all_cities()
-print("\n🔎 Finding a city:")
-print(find_city(MUMBAI))
-
-print("\n📏 Distance between Delhi and Mumbai:")
-print(calculate_distance(DELHI, MUMBAI), "km")
