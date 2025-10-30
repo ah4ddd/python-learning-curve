1744dd8c247202de55700508e5234ca19f3c8ca7
Ahad <mohdahad60080@gmail.com>
2025-10-30 12:20:16 +0530

    list in tupes in list

--- tuples_dict_sets/tuples_dict_sets.py
+++ tuples_dict_sets/tuples_dict_sets.py
@@ -1,21 +1,31 @@
-game = ("sekiro", "elden ring", "mgs")
+# Album structure:
+# (album_name, artist, year, [list of songs])
 
-game_list = list(game)
+music_library = [
+    ("Dark Side of the Moon", "Pink Floyd", 1973,
+     ["Speak to Me", "Breathe", "Time", "Money"]),
 
-print(game_list)
+    ("Thriller", "Michael Jackson", 1982,
+     ["Wanna Be Startin' Somethin'", "Thriller", "Beat It"]),
 
-game = ("sekiro", "elden ring", "mgs")
+    ("OK Computer", "Radiohead", 1997,
+     ["Paranoid Android", "Karma Police", "No Surprises"])
+]
 
-game_list = list(game)
+# Function to add a song to an album:
+def add_song_to_album(library, album_name, new_song):
+    for album in library:
+        if album[0] == album_name:
+            album[3].insert(0, new_song)  # album[3] is the song list
+            print(f"✅ Added '{new_song}' to {album_name}")
+            return
+    print(f"❌ Album '{album_name}' not found")
 
-print(game_list)
+# Add a song:
+add_song_to_album(music_library, "Thriller", "Billie Jean")
 
-game_list[1] = "silent_hill"
-
-game = tuple(game_list)
-
-print(game)
-
-game = tuple(game_list)
-
-print(game)
+# Check it:
+for album in music_library:
+    if album[0] == "Thriller":
+        print(album[3])
+# ['Wanna Be Startin' Somethin'', 'Thriller', 'Beat It', 'Billie Jean']
