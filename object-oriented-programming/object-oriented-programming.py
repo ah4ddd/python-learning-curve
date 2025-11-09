f17d2d7a3a67b48783d869d008574fbff781af4d
Ahad <mohdahad60080@gmail.com>
2025-11-09 19:08:57 +0530

    ohman

--- object-oriented-programming/object-oriented-programming.py
+++ object-oriented-programming/object-oriented-programming.py
@@ -1,1 +1,8 @@
-class BankAccount:
+class Car:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+        self.speed = 0
+
+    def accelerate(self):
+        self.speed += 10
