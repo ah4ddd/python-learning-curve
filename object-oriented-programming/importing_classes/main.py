e4e3389a1b65774d0ff59952095ade2c31248bb8
Ahad <mohdahad60080@gmail.com>
2025-11-14 17:12:34 +0530

    yeahh

--- object-oriented-programming/importing_classes/main.py
+++ object-oriented-programming/importing_classes/main.py
@@ -1,11 +1,15 @@
 from car import Car as Cat
 import vehicle as vcat
 from electric_car import ElectricCar as Ecat
+from garage import Garage
 
+my_garage = Garage("Ahad's Garage")
 my_car = Cat("Honda", "Civic", 2020)
 my_bike = vcat.Motorcycle("Harley", "Sportster", 2019)
 my_truck = vcat.Truck("Ford", "F-150", 2021)
 my_tesla = Ecat("Tesla", "Model 3", 2023)
+my_garage.add_vehicle(my_car)
+my_garage.add_vehicle(my_truck)
 
 print(my_car.get_descriptive_name())
 my_car.read_odometer(50)
@@ -19,3 +23,5 @@ print(my_tesla.get_descriptive_name())
 my_tesla.battery.describe_battery()
 my_tesla.battery.get_range()
 my_tesla.charge()
+
+my_garage.list_vehicles()
