af945ffbe85d9dd05ae8e480bcaf59dd78d283af
Ahad <mohdahad60080@gmail.com>
2025-11-19 13:09:05 +0530

    i_

--- comprehension/comprehension.py
+++ comprehension/comprehension.py
@@ -6,18 +6,22 @@ films = [
     ("Paris Texas", "Wim Wenders", 1984, 9.5)
 ]
 
+cinema = [f for f in films]
 titles = [f[0] for f in films]
 directors = {f[1] for f in films}
+years = {f[2] for f in films}
 rating_map = {title: rating for title, _, _, rating in films}
 high_rated = [f for f in films if f[3] >= 9.5]
 initials = [director.split()[0][0] for _, director, _, _ in films]
 flat_data = [item for movie in films for item in movie]
 rating_pairs = ((title, rating) for title, _, _, rating in films)
 
-print("Titles:", titles)
-print("Unique Directors:", directors)
-print("Title → Rating:", rating_map)
-print("Masterpieces:", high_rated)
-print("Director Initials:", initials)
-print("Flattened Data:", flat_data)
-print("Generator Output:", list(rating_pairs))
+print(f"Films : {cinema}\n")
+print(f"Titles: {titles}\n")
+print(f"Directors:, {directors}")
+print(f"Year : {years}\n")
+print(f"Title → Rating:, {rating_map}\n")
+print(f"Masterpieces: {high_rated}\n")
+print(f"Director Initials: {initials}\n")
+print(f"Flattened Data: {flat_data}\n")
+print(f"Generator Output: {list(rating_pairs)}\n")
