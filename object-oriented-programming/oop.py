e72b8175eda371d4d0eb33ec9fc556ee759dc0c9
Ahad <mohdahad60080@gmail.com>
2025-12-02 16:37:41 +0530

    just iter

--- object-oriented-programming/oop.py
+++ object-oriented-programming/oop.py
@@ -5,7 +5,15 @@ class Bag:
     def __reversed__(self):
         return reversed(self.items)
 
+    def __iter__(self):
+        return iter(self.items)
+
+
 b = Bag(["knife", "rope", "key"])
 
 for item in reversed(b):
     print(item)
+
+for item in b:
+    print(item)
+
