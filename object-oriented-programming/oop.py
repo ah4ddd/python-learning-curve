afbbbb0c17f97575362f25f173823b49eb59b7b5
Ahad <mohdahad60080@gmail.com>
2025-12-03 17:32:44 +0530

    setattr

--- object-oriented-programming/oop.py
+++ object-oriented-programming/oop.py
@@ -1,8 +1,15 @@
-class A:
-    pass
+class User:
+    def __setattr__(self, name, value):
+        if isinstance(value, str):
+            value = value.strip()
 
-obj = A()
-obj.name = "Ahad"
-print(obj.name)
+        if name == "age":
+            value = int(value)
 
+        super().__setattr__(name, value)
 
+u = User()
+u.name = "    Ahad    "
+u.age = "25"
+print(u.name)
+print(u.age)
