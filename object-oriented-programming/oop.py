944d5c752473850b43728f3abc1bd46a183445f9
Ahad <mohdahad60080@gmail.com>
2025-12-09 22:54:38 +0530

    proj

--- object-oriented-programming/oop.py
+++ object-oriented-programming/oop.py
@@ -1,21 +1,84 @@
-from abc import ABC, abstractmethod
+from random import randint, choice
 
-class Weapon(ABC):
+class DiceRoll:
+    def __init__(self, sides=6):
+        self.sides = sides
 
-    @abstractmethod
-    def attack(self):
-        pass
+    def roll_dice(self):
+        roll = randint(1, self.sides)
+        return roll
 
-class Sword(Weapon):
-    def attack(self):
-        return "Slash!"
+    def roll_many_times(self, rolls = 10):
+        for _ in range(rolls):
+            roll = self.roll_dice()
+            print(roll)
+        return None
 
-class Gun(Weapon):
-    def attack(self):
-        return "Bang!"
+dice = DiceRoll()
+ten_sided = DiceRoll(10)
+twenty_sided  = DiceRoll(20)
 
-weapon1 = Sword()
-weapon2 = Gun()
+print(dice.roll_dice())
+print()
+dice.roll_many_times()
 
-print(weapon1.attack())
-print(weapon2.attack())
+print()
+ten_sided.roll_many_times()
+
+print()
+twenty_sided.roll_many_times()
+print()
+
+class Lottery:
+    def __init__(self, words, numbers):
+        self.words = words
+        self.numbers = numbers
+        self.lucky_words = ("Python", "C++")
+        self.lucky_numbers = (1, 10, 100)
+
+    def word_ticket(self):
+        pick = choice(self.words)
+        if pick in self.lucky_words:
+            print(f"Your Luck : {pick}\nYou won Free book on {pick} programming\n")
+        else:
+            print(f"You got {pick}\nsee you next time\n")
+
+    def number_ticket(self):
+        pick = choice(self.numbers)
+        if pick in self.lucky_numbers:
+            print(f"Lucky Number : {pick}\nyou won 10000$\n")
+        else:
+            print(f"Aw, so close : {pick}\nbetter luck next time\n")
+
+    def until_word_win(self):
+        tries = 0
+        while True:
+            tries += 1
+            if choice(self.words) in self.lucky_words:
+                print(f"Winning Word after {tries} tries\n")
+                break
+
+    def until_number_win(self):
+        tries = 0
+        while True:
+            tries += 1
+            if choice(self.numbers) in self.lucky_numbers:
+                print(f"Winning Number after {tries} tries\n")
+                break
+
+lottery_word = ["Python", "Java", "JS", "C++", "Rust"]
+lottery_number = (67, 70, 10, 3, 5, 6, 90, 75, 100, 1)
+
+
+game = Lottery(lottery_word, lottery_number)
+
+print("--- Lucky Words : Python, C++ ---")
+print("--- Lucky Numbers : 1, 10, 100 ---\n")
+
+game.word_ticket()
+game.number_ticket()
+
+game.until_word_win()
+game.until_number_win()
+
+
