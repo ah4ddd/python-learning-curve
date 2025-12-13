3c0b25cbb5565ffda75849202bb6de82cd6bdf69
Ahad <mohdahad60080@gmail.com>
2025-12-13 22:21:53 +0530

    inv

--- files_and_exceptions/fae.py
+++ files_and_exceptions/fae.py
@@ -20,4 +20,8 @@ bday = input("Enter Bday in mmddyy: ")
 if bday in pi_string:
     print("oh your bday is in first million digit of pi, not impressesed")
 else:
-    print("haha, no nicht nada nothing, pi says bye")
+    print("haha, no nicht nada nothing.. pi says bye")
+
+# Read name from file
+with open("user.txt", "r") as f:
+    user_name = f.read()
