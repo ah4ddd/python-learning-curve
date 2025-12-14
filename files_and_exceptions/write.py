9354e935c488f514eede65fc33bb6cc9786dfe7b
Ahad <mohdahad60080@gmail.com>
2025-12-14 16:22:56 +0530

    write

--- files_and_exceptions/write.py
+++ files_and_exceptions/write.py
@@ -5,6 +5,30 @@ with open(file, "w") as f:
     f.write("This is my freedom\n")
 
 
-with open(file, 'a') as file_object:
+with open(file, "a") as file_object:
     file_object.write("I also love finding meaning in large datasets.\n")
     file_object.write("I love creating apps that can run in a browser.\n")
+
+print("=== Guest Log ===")
+print("Enter 'N' when you are done with your lil list\n")
+
+while True :
+    name = input("Put your name:").lower()
+    more = input("you wanna put more name ? y/n:")
+    with open("guest.txt", "a") as g:
+        g.write(f"{name.title()}\n")
+    if more == "n":
+        break
+
+print()
+
+print("=== Programming Poll ===")
+print("Enter 'n' when you are done with your lil torcher session\n")
+
+while True :
+    why = input("Why you even like programming ( i mean not touching grass)?").lower()
+    more_why = input("you wanna put more reason or touch grass.. ? y/n:")
+    with open("guest.txt", "a") as p:
+        p.write(f"Why likes Programming: {why.title()}\n")
+    if more_why == "n":
+        break
