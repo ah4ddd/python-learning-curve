b4f5c506b7ec06cc4cef502aa27e25ad531c1ade
Ahad <mohdahad60080@gmail.com>
2025-12-16 15:18:39 +0530

    ohya

--- files_and_exceptions/fae.py
+++ files_and_exceptions/fae.py
@@ -1,10 +1,20 @@
 from pathlib import Path
 
-file_path = Path("data") / "projects" / "report.txt"
+file_path = Path("notes.txt")
 
-print(f"Full path: {file_path}")
-print(f"Name: {file_path.name}")           # Filename
-print(f"Stem: {file_path.stem}")           # Filename without extension
-print(f"Suffix: {file_path.suffix}")       # Extension
-print(f"Parent: {file_path.parent}")       # Parent directory
-print(f"Parents: {list(file_path.parents)}")  # All parents
+# Check if exists
+if file_path.exists():
+    print("✅ File exists!")
+else:
+    print("❌ File doesn't exist!")
+
+# Check if it's a file
+if file_path.is_file():
+    print("✅ its a file\nContents:")
+    with open("notes.txt", "r") as n:
+        notes = n.read()
+        print(notes.rstrip())
+
+# Check if it's a directory
+if file_path.is_dir():
+    print("✅ It's a directory!")
