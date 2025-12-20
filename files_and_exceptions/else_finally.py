6c2e8a2f4efdc58b3c78eba9d17871fc0b615926
Ahad <mohdahad60080@gmail.com>
2025-12-20 22:54:32 +0530

    testing democtx

--- files_and_exceptions/else_finally.py
+++ files_and_exceptions/else_finally.py
@@ -1,4 +1,14 @@
-with open("data.json")as f:
-    read = f.read()
+class DemoContext:
+    def __enter__(self):
+        print("🟢 ENTER: setup happens")
+        return "RESOURCE"
 
-print(read)
+    def __exit__(self, exc_type, exc_value, traceback):
+        print("🔴 EXIT: cleanup happens")
+        print(f"   exc_type = {exc_type}")
+        print(f"   exc_value = {exc_value}")
+        print("   resource released")
+
+with DemoContext() as d:
+    print("using",d)
+
