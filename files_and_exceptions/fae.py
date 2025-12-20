a4d6aa3abdc1cb710cc3e0db55809207ebf84bcc
Ahad <mohdahad60080@gmail.com>
2025-12-20 19:46:13 +0530

    better

--- files_and_exceptions/fae.py
+++ files_and_exceptions/fae.py
@@ -2,5 +2,11 @@ import array
 
 a = array.array('i',[1,2,3,4,5])
 l = len(a)
+print(l)
 for i in range(0, l):
     print("ELEMENT ", i, " is ", a[i])
+
+a = [1, 2, 3, 4, 5]
+
+for i, value in enumerate(a):
+    print(f"ELEMENT {i} is {value}")
