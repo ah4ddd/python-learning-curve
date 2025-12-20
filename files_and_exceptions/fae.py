bacf64ffb8a002d94038335c056dfbecf00f8678
Ahad <mohdahad60080@gmail.com>
2025-12-20 21:00:37 +0530

    else show intent

--- files_and_exceptions/fae.py
+++ files_and_exceptions/fae.py
@@ -1,17 +1,6 @@
-print("Give me two numbers, and i'll divide them")
-print("Enter 'q' to quit.")
-
-while True:
-    first_number = input("\nFirst number: ")
-    if first_number == "q":
-        break
-    second_number = input("\nSecond number: ")
-    if second_number == "q":
-        break
-    try:
-        answer = int(first_number) / int(second_number)
-    except ZeroDivisionError:
-        print("You can't divide by Zero")
-    else:
-        print(answer)
-
+try:
+    age = int(input("Enter your age: "))
+except ValueError:
+    print("❌ Invalid input!")
+else:
+    print(f"✅ Valid age: {age}")
