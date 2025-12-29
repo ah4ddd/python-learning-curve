dfb6ac5c11756a2d4da170708904976418b12b29
Ahad <mohdahad60080@gmail.com>
2025-12-29 21:46:01 +0530

    pass

--- files_and_exceptions/fae.py
+++ files_and_exceptions/fae.py
@@ -2,16 +2,16 @@ from pathlib import Path
 
 def count_words(path):
     try:
-        contents = path.read_text(encoding='utf-8')
+        contents = path.read_text(encoding="utf-8")
     except FileNotFoundError:
-        print(f"Sorry, the file {path} does not exist.")
+        pass
+    else:
+        words = contents.split()
+        num_words = len(words)
 
-    words = contents.split()
-    num_words = len(words)
+        print(f"The file {path} has about {num_words} words")
 
-    print(f"The file {path} has about {num_words} words")
-
-literatures = ['crimeandpunishment.txt', 'metamorphosis.txt']
+literatures = ['crimeandpunishment.txt', 'metamorphosis.txt',"fuck.txt"]
 
 for books in literatures:
     path = Path(books)
