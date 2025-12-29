6863cfd734938427488cc4da648c529d7607e110
Ahad <mohdahad60080@gmail.com>
2025-12-29 21:29:49 +0530

    dual read

--- files_and_exceptions/fae.py
+++ files_and_exceptions/fae.py
@@ -1,12 +1,18 @@
 from pathlib import Path
 
-try:
-    path = Path('crimeandpunishment.txt')
-    contents = path.read_text(encoding='utf-8')
-except FileNotFoundError:
-    print(f"Sorry, the file {path} does not exist.")
+def count_words(path):
+    try:
+        contents = path.read_text(encoding='utf-8')
+    except FileNotFoundError:
+        print(f"Sorry, the file {path} does not exist.")
 
-words = contents.splitlines()
-num_words = len(words)
+    words = contents.split()
+    num_words = len(words)
 
-print(f"The file {path} has about {num_words} lines")
+    print(f"The file {path} has about {num_words} words")
+
+literatures = ['crimeandpunishment.txt', 'metamorphosis.txt']
+
+for books in literatures:
+    path = Path(books)
+    count_words(path)
