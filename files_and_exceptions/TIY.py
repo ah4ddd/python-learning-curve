4bb878ea8d2298ab39c3da30288f85a1ca20817e
Ahad <mohdahad60080@gmail.com>
2025-12-29 23:30:01 +0530

    far better

--- files_and_exceptions/TIY.py
+++ files_and_exceptions/TIY.py
@@ -1,3 +1,16 @@
+from pathlib import Path
+
+paths = ['film.txt', 'tv.txt', 'dontknow.txt']
+
+for path in paths:
+    try:
+        with open(path, "r") as f:
+            content = f.read()
+        print(f"\n--- {path} ---")
+        print(content)
+    except FileNotFoundError:
+        print(f"❌ file {path} not found")
+
 while True:
     try:
         numb_one = int(input("first number: "))
@@ -6,4 +19,3 @@ while True:
         print("❌ Please enter numbers only.")
     else:
         print(numb_one + numb_two)
-
