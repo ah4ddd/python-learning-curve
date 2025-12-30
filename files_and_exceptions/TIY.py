3a020b3810920519176c14fd642d2d7a4db24b1e
Ahad <mohdahad60080@gmail.com>
2025-12-30 20:25:04 +0530

    nitpicker

--- files_and_exceptions/TIY.py
+++ files_and_exceptions/TIY.py
@@ -4,25 +4,27 @@ import json
 path = Path('favnumb.json')
 
 def get_new_number(path):
-    numb = int(input("What is your favorite number? "))
-    contents = json.dumps(numb)
-    path.write_text(contents)
+    while True:
+        try:
+            numb = int(input("What is your favorite number? "))
+            break
+        except ValueError:
+            print("Please enter a valid integer.")
+
+    path.write_text(json.dumps(numb))
     return numb
 
 def stored_number(path):
     if path.exists():
-        contents = path.read_text()
-        numb = json.loads(contents)
-        return numb
-    else:
-        return None
+        return json.loads(path.read_text())
+    return None
 
 def fav_number():
     numb = stored_number(path)
-    if numb:
-        print(f"you already gave me your favorite number, its: {numb}!")
+    if numb is not None:
+        print(f"I know your favorite number! It's {numb}.")
     else:
         numb = get_new_number(path)
-        print(f"I'll remember your favorite number when you comeback: {numb}!")
+        print(f"I'll remember your favorite number when you come back: {numb}.")
 
 fav_number()
