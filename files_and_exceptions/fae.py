da3060db6ef8a4edd2fecda412462ac3942dec65
Ahad <mohdahad60080@gmail.com>
2025-12-30 17:09:12 +0530

    that all ?

--- files_and_exceptions/fae.py
+++ files_and_exceptions/fae.py
@@ -1,7 +1,5 @@
-from multiprocessing import get_start_method
 from pathlib import Path
 import json
-
 
 path = Path('numbers.json')
 contents = path.read_text()
@@ -9,6 +7,12 @@ numbers = json.loads(contents)
 print(contents)
 
 path = Path('username.json')
+
+def get_new_user(path):
+    username = input("What is your name? ")
+    contents = json.dumps(username)
+    path.write_text(contents)
+    return username
 
 def stored_user(path):
     if path.exists():
@@ -26,9 +30,7 @@ def greet():
     if username:
         print(f"Welcome back, {username}!")
     else:
-        username = input("What is your name? ")
-        contents = json.dumps(username)
-        path.write_text(contents)
+        username = get_new_user(path)
         print(f"we'll remembe you when you comeback, {username}!")
 
 greet()
