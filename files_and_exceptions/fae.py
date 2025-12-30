08f13e5af0a3050fa6ebeb9331f9de96f6bf1c91
Ahad <mohdahad60080@gmail.com>
2025-12-30 15:55:03 +0530

    get it now

--- files_and_exceptions/fae.py
+++ files_and_exceptions/fae.py
@@ -1,29 +1,7 @@
-literatures = ['crimeandpunishment.txt', 'metamorphosis.txt']
-for i,book in enumerate(literatures):
-    print(f"{i+1}: {book}")
+data = (1, (10, 20))
 
-def how_many_times(path, search):
-    with open(path, "r", encoding="utf-8") as f:
-        book = f.read().lower()
-        words = book.split()
-        return words.count(search.lower())
+a, (b, c) = data
 
-try:
-    file = int(input("Choose book file, via number: "))
-    name = input("Enter a character name: ")
-    if file == 1:
-        file = "crimeandpunishment.txt"
-    elif file == 2:
-        file = "metamorphosis.txt"
-    else:
-        print("Invalid choice")
-        raise ValueError
-
-    count = how_many_times(file, name)
-    print(f"{name.title()} appears {count} times.")
-
-except FileNotFoundError:
-    print(f"❌ file does not exist : {file} ❌")
-except ValueError:
-    print("❌ Please enter a valid number ❌")
-
+print(a)
+print(b)
+print(c)
