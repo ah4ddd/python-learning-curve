41cd3651c72cce61883adf7655136a9d839ef549
Ahad <mohdahad60080@gmail.com>
2025-12-30 17:04:07 +0530

    double function

--- files_and_exceptions/fae.py
+++ files_and_exceptions/fae.py
@@ -1,9 +1,34 @@
+from multiprocessing import get_start_method
 from pathlib import Path
 import json
 
-numbers = [1,2,3,4,5,6,7,8]
 
 path = Path('numbers.json')
-contents = json.dumps(numbers)
-path.write_text(contents)
+contents = path.read_text()
+numbers = json.loads(contents)
+print(contents)
 
+path = Path('username.json')
+
+def stored_user(path):
+    if path.exists():
+        contents = path.read_text()
+        username = json.loads(contents)
+        return username
+    else:
+        return None
+
+def greet():
+    """
+    greet user by name
+    """
+    username = stored_user(path)
+    if username:
+        print(f"Welcome back, {username}!")
+    else:
+        username = input("What is your name? ")
+        contents = json.dumps(username)
+        path.write_text(contents)
+        print(f"we'll remembe you when you comeback, {username}!")
+
+greet()
