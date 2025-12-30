38aba8b7216073b932f4bd4bb36b5afa16e6d0a0
Ahad <mohdahad60080@gmail.com>
2025-12-30 13:28:02 +0530

    line reading

--- files_and_exceptions/file_searcher.py
+++ files_and_exceptions/file_searcher.py
@@ -4,17 +4,30 @@ for i, book in enumerate(literatures, start=1):
     print(f"{i}: {book}")
 
 def how_many_times(path, search):
+    matches = []
+
     with open(path, "r", encoding="utf-8") as f:
-        return f.read().lower().split().count(search.lower())
+        for line_number, line in enumerate(f, start=1):
+            if search.lower() in line.lower():
+                matches.append((line_number, line.strip()))
+        return matches
 
 try:
     choice = int(input("Choose book file, via number: "))
     name = input("Enter a character name: ")
 
     file = literatures[choice - 1]
-    count = how_many_times(file, name)
-
-    print(f"{name.title()} appears {count} times in {file}")
+    results = how_many_times(file, name)
+    if file == "crimeandpunishment.txt":
+        file = "Crime and Punishment"
+    elif file == "metamorphosis.txt":
+        file = "Metamorphosis"
+    count = len(results)
+    print(f"\n{name.title()} appears on {count} lines in {file}.\n")
+    for idx, (line_number,text) in enumerate(results, start=1):
+        print(f"{idx}. Line {line_number}:")
+        print(text)
+        print()
 
 except (ValueError, IndexError):
     print("❌ Invalid choice ❌")
