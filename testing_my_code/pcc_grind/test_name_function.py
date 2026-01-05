5fd2ce45df6431b1880c7c6d67d8bf77a1cd22c2
Ahad <mohdahad60080@gmail.com>
2026-01-05 23:10:51 +0530

    enough for today

--- testing_my_code/pcc_grind/test_name_function.py
+++ testing_my_code/pcc_grind/test_name_function.py
@@ -1,4 +1,5 @@
 from name_function import get_formatted_name
-def test_first_last_name():
-    formatted_name = get_formatted_name("Bob", "Dylan")
-    assert formatted_name == 'Bob Dylan'
+def test_first_last_middle_name():
+    formatted_name = get_formatted_name("Bob", "Dylan","Song")
+    assert formatted_name == 'Bob Song Dylan'
+
