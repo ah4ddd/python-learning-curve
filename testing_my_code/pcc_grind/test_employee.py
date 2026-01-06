ea6b9a3078e1ac5139a57851f07a93d4b814c2c4
Ahad <mohdahad60080@gmail.com>
2026-01-06 16:43:18 +0530

    sharper more cringer

--- testing_my_code/pcc_grind/test_employee.py
+++ testing_my_code/pcc_grind/test_employee.py
@@ -1,17 +1,28 @@
 import pytest
 from employee import Employee
 
+
 @pytest.fixture
-def employee():
-    salary_raise = Employee('Ahad', 'Kin', 256000)
-    return salary_raise
+def employee_with_salary():
+    return Employee('Ahad', 'Kin', 256000)
 
-def test_get_default_raise(employee):
-    new_salary = employee.get_raise()
-    assert new_salary == 261000
-    assert employee.annual_salary == 261000
 
-def test_custom_raise(employee):
-    new_salary = employee.get_raise(44000)
-    assert new_salary == 300000
-    assert employee.annual_salary == 300000
+def test_get_default_raise(employee_with_salary):
+    result = employee_with_salary.get_raise()
+
+    assert result == 261000
+    assert employee_with_salary.annual_salary == 261000
+
+
+def test_get_custom_raise(employee_with_salary):
+    result = employee_with_salary.get_raise(44000)
+
+    assert result == 300000
+    assert employee_with_salary.annual_salary == 300000
+
+
+def test_get_raise_mutates_state(employee_with_salary):
+    employee_with_salary.get_raise()
+    employee_with_salary.get_raise()
+
+    assert employee_with_salary.annual_salary == 266000
