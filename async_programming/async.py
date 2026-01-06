2590aaa44f4b8f77d70ddb52ee9a510fdf812501
Ahad <mohdahad60080@gmail.com>
2026-01-06 21:03:28 +0530

    fucking vague

--- async_programming/async.py
+++ async_programming/async.py
@@ -1,10 +1,15 @@
 import asyncio
 
-async def task():
-    await asyncio.sleep(2)
-    print("done")
+async def task(name):
+    print(f"Starting {name}")
+    await asyncio.sleep(2)   # non-blocking wait
+    print(f"Finished {name}")
 
 async def main():
-    await asyncio.gather(task(), task())
+    await asyncio.gather(
+        task("A"),
+        task("B")
+    )
 
 asyncio.run(main())
+
